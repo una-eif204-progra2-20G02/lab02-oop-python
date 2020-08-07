23826e74a4441cf57aab7fc68abb87b9ac9bff09
Fabi2094 <68891397+Fabi2094@users.noreply.github.com>
2020-08-07 10:45:34 -0600

    Update University.h

--- University.h
+++ University.h
@@ -10,6 +10,41 @@
 #include "Administrative.h"
 
 class University {
+private:
+    string name;
+   /*
+    * Professor professor;
+    * Administrative administrative;
+    * vector<Professor*> professorList;
+    * vector<Administrative*> administrativeList;
+    */
+
+public:
+
+    /*University();
+    University(const string& name);
+    University(const string &name, const Professor &professor, const Administrative &administrative,const vector<Professor *> &professorList, const vector<Administrative *> &administrativeList);
+
+    string getName();
+    void setName(const string &name);
+
+    string getProfessor();
+    void setProfessor(const Professor &professor);
+
+    const Administrative getAdministrative();
+    void setAdministrative(const Administrative &administrative);
+
+    void addAdministrative(Administrative*);
+    vector<Administrative>* getListAdministrative();
+
+    void addProfessor(Professor*);
+    vector<Professor>* getListProfessor();
+
+    void AddProfessor(Professor*);
+    void AddAdministrative(Administrative*);
+
+    */
+
 
 };
 
