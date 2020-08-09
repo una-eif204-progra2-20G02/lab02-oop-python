0c34b98bc32da1305f45ede23763f49acaa39f53
Fabi2094 <68891397+Fabi2094@users.noreply.github.com>
2020-08-09 11:40:23 -0600

    Update main
    Creacion del main entre comentarios falta  terminar otras clases

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,43 @@
 //
 // Created by Maikol Guzman on 8/2/20.
 //
-
-#include "Administrative.h"
-#include "Professor.h"
-
 #include<iostream>
 using namespace std;
 
-int main() {
-    //testing
+#include "Person.h"
+#include "Professor.h"
+#include "Administrative.h"
+#include "University.h"
 
-    Person person("Diana", "Fallas", 117770654);
+
+int main() {
+
+/*
+    University* university=new University("University");
+    Professor* professor1 = new Professor();
+    Professor* professor2 = new Professor();
+    Administrative* administrative1= new Administrative();
+    Administrative* administrative2= new Administrative();
+
+    university->addProfessor(professor1);
+    university->addProfessor(professor2);
+    university->addAdministrative(administrative1);
+    university->addAdministrative(administrative2);
+
+
+    system("pause");
+    return 0;
+
+
+*/
+    //testing
+    /*Person person("Diana", "Fallas", 117770654);
     cout << person.toString() << endl;
 
     Administrative admin("Esperancita", "Gutierrez", 123456789, 350000);
     cout << admin.toString() << endl;
 
     Professor profe("Manuel", "Sutano", 123456789, 350000, 156877);
-    cout << profe.toString();
+    cout << profe.toString();*/
 
 }
