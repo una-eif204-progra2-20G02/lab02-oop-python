3b7e5cdac78592fcc2155ca207a6eb11acf3c6d5
Diana Fallas <65523878+dianattrix@users.noreply.github.com>
2020-08-11 10:36:28 -0600

    University

--- University.cpp
+++ University.cpp
@@ -5,55 +5,66 @@
 
 #include "University.h"
 
-University::University() {
 
+University::University(){
+    this->name="";
 }
 
-University::University(const string &name) {
-
+University::University(const string& name){
+    this->name=name;
 }
 
-University::University(const string &name, const Professor &professor, const Administrative &administrative,
-                       const vector<Professor> &professorList, const vector<Administrative> &administrativeList) {
-
+University::University(const string &name, const Professor &professor, const Administrative &administrative,const vector<Professor> &professorList, const vector<Administrative> &administrativeList){
+    this->name  = name;
+    this->professor = professor;
+    this->administrative = administrative;
+    this->professorList = professorList;
+    this->administrativeList = administrativeList;
+}
+string University::getName(){
+    return name;
 }
 
-string University::getName() {
-    return std::string();
+void University::setName(const string &name){
+    this->name = name;
 }
 
-void University::setName(const string &name) {
-
+Professor University::getProfessor(){
+    return professor;
 }
 
-Professor University::getProfessor() {
-    return Professor();
+void University::setProfessor(const Professor &professor){
+    this->professor = professor;
 }
 
-void University::setProfessor(const Professor &professor) {
-
+Administrative University::getAdministrative(){
+    return administrative;
 }
 
-Administrative University::getAdministrative() {
-    return Administrative();
+void University::setAdministrative(const Administrative &administrative){
+    this->administrative = administrative;
 }
 
-void University::setAdministrative(const Administrative &administrative) {
-
+void University::getListProfessor(){
+    int i = 0;
+    while(i < professorList.size()) {
+        cout << professorList.at(i).toString();
+        i++;
+    }
 }
 
-void University::addAdministrative(Administrative) {
+void University::addProfessor(Professor professor){
+    professorList.push_back(professor);
+}
 
+void University::addAdministrative(Administrative administrative) {
+    administrativeList.push_back(administrative);
 }
 
 void University::getListAdministrative() {
-
-}
-
-void University::addProfessor(Professor) {
-
-}
-
-void University::getListProfessor() {
-
+    int i = 0;
+    while(i < administrativeList.size()) {
+        cout << administrativeList.at(i).toString();
+        i++;
+    }
 }
